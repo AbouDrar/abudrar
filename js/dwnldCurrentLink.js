20e931ed6276491c572ac9f3a6b6bb9a0b92f28a
AbouDrar <AbouDrar@users.noreply.github.com>
2021-10-21 00:22:33 +0000

    Update dwnldCurrentLink.js

--- js/dwnldCurrentLink.js
+++ js/dwnldCurrentLink.js
@@ -5,7 +5,7 @@
         var website = e.target.getAttribute( 'data-site' );
         var slug = e.target.getAttribute( 'data-url' );
     //now we need to send the user to the static page: 
-    var downloadpage = "/tests/download.html?website=" + website + "&slug=" + slug;
+    var downloadpage = "/p/download.html?website=" + website + "&slug=" + slug;
     window.location = downloadpage;
     });
 
