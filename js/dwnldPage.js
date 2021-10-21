abd443198df7a8d2b19411de6df634eef5078c4c
AbouDrar <AbouDrar@users.noreply.github.com>
2021-10-21 20:14:33 +0000

    Update dwnldPage.js

--- js/dwnldPage.js
+++ js/dwnldPage.js
@@ -1,37 +1,36 @@
-//<![CDATA[
+
 var seconds = 15; // Total seconds to wait
 function download_timer(){
         seconds = seconds - 1;
-        if (seconds < 0) {
+     if (seconds < 0) {
 			// get the query string part from url
-			var query = window.location.search;
+			var query = window.location.search; // get the query search part if the url
 			var urlParams = new URLSearchParams( query ); // object of the parameters
-			
-			for (var theParameter in urlParams) {
-				if ( urlParams.hasOwnProperty( theParameter ) ) {
-					/**** if(){
-						
-					} **/
-					
-        console.log(theParameter + " ---> " + urlParams[theParameter]);
-    }
-}
-			var website = urlParams.get( 'website' );
-			var slug = urlParams.get( 'slug' );
-			var newSlug = slug.substring( 11 ); //delete the first 11 characters of the slug.
-			var TheFullDownloadLink = website + newSlug;
+			var TheFullDownloadLink = ""; //link is empty now
+			for(var [param, value] of urlParams.entries()) {
+				//check if the param is the slug an trim it:
+				// do not spell "website" nor "slug"
+				if( param === "website" ){
+					TheFullDownloadLink += value;
+				}else if( param === "slug" ) {
+					value = value.substring( 12 ); // remove the first 12 characters of slug's value 
+					TheFullDownloadLink += value;
+				}else if ( param === "m" && value === 1 ){ //don't put mobile version in the url
+					TheFullDownloadLink += "";
+				}else{
+					TheFullDownloadLink += "&" + param + "=" + value;
+				}
+			}
 			if( TheFullDownloadLink != null){
-				window.location = TheFullDownloadLink;
+				window.location = TheFullDownloadLink; // go to download link 
 			}else{
 				document.getElementById("Countdown").innerHTML = "لا يعمل هذا الرابط";
 				}
 				
-        } else {
+      } else {
             // Update remaining seconds
             document.getElementById("Countdown").innerHTML = "سيتم توجيهك لصفحة التحميل بعد " +seconds+ " ثانية.";
-            // Countdown wait time is 1 second
+            // Countdown wait time is 1 second = 1000 miliseconds
             window.setTimeout( "download_timer()" , 1000);
         }
    }
-
-//]]>
