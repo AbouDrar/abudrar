360b83605daa4d07e981515079b234b3ba2ec43b
AbouDrar <AbouDrar@users.noreply.github.com>
2021-10-21 01:12:49 +0000

    Update dwnldPage.js

--- js/dwnldPage.js
+++ js/dwnldPage.js
@@ -1,25 +1,27 @@
-function download_timer() {
+//<![CDATA[
+function download_timer(){
 		var seconds = 15; // Total seconds to wait
         seconds = seconds - 1;
         if (seconds < 0) {
-	   // get the query string part from url
-	   var query = window.location.search;
-	   var urlParams = new URLSearchParams( query ); // object of the parameters
-	   var website = urlParams.get( 'website' );
-	   var slug = urlParams.get( 'slug' );
-	   var newSlug = slug.substring( 11 ); //delete the first 11 characters of the slug.
-	   var TheFullDownloadLink = website + newSlug;
-       if( TheFullDownloadLink != null){
-            window.location = TheFullDownloadLink;
-       }else{
-           //if there is no link in local storage:
-           document.getElementById("Countdown").innerHTML = "لا يعمل هذا الرابط";
-       }
-           
+			// get the query string part from url
+			var query = window.location.search;
+			var urlParams = new URLSearchParams( query ); // object of the parameters
+			var website = urlParams.get( 'website' );
+			var slug = urlParams.get( 'slug' );
+			var newSlug = slug.substring( 11 ); //delete the first 11 characters of the slug.
+			var TheFullDownloadLink = website + newSlug;
+			if( TheFullDownloadLink != null){
+				window.location = TheFullDownloadLink;
+			}else{
+				document.getElementById("Countdown").innerHTML = "لا يعمل هذا الرابط";
+				}
+				
         } else {
             // Update remaining seconds
             document.getElementById("Countdown").innerHTML = "سيتم توجيهك لصفحة التحميل بعد " +seconds+ " ثانية.";
             // Countdown wait time is 1 second
             window.setTimeout(download_timer(), 1000);
         }
-    }
+   }
+
+//]]>
